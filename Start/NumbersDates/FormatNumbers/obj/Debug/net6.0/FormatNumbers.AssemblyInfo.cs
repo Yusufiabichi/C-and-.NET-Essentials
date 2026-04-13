1548503cee218fe3b39d97bc5b7a44049a11a05c
Yusufiabichi <yusufiabichi@yahoo.com>
2026-04-13 20:58:14 +0100

    Regex Finding

--- Start/NumbersDates/FormatNumbers/obj/Debug/net6.0/FormatNumbers.AssemblyInfo.cs
+++ Start/NumbersDates/FormatNumbers/obj/Debug/net6.0/FormatNumbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormatNumbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da85122eba93c15e8cb605f8d4ab7b240ea09db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abfac1e22cc0daa3e3e59887008142e9772bec4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormatNumbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormatNumbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
